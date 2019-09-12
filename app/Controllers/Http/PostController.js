40313819597abbe581d305e532559486c19a685a
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-09-12 12:09:45 +0700

    passing array to view

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -5,8 +5,14 @@ class PostController {
   //   return 'Posts'
   // }
   async index({ view }){
+    const posts = [
+      {title: 'Post One', body: 'This is post one'},
+      {title: 'Post Two', body: 'This is post two'},
+      {title: 'Post Three', body: 'This is post three'},
+    ]
     return view.render('posts.index',{
-      title: 'Latest Post'
+      title: 'Latest Post',
+      posts: posts
     })
   }
 }
